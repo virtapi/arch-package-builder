efc4d26bae3534aadb2ad513dc3b37567f32d4fa
Tim Meusel <tim@bastelfreak.de>
2016-03-07 16:57:29 +0100

    update create script
    wörks

--- create_subjobs.groovy
+++ create_subjobs.groovy
@@ -1,11 +1,16 @@
-new File('aur-packages').eachLine { line ->
-  println(line)
-	job(line) {
-		scm {
-			git("https://aur.archlinux.org/${line}")
-		}
-		steps {
-			println("makechrootpkg -c -r /mnt/aur/build_test")
-		}
-	}
+def PackagesFile = new File('/var/lib/jenkins/jobs/Arch Package Builder/workspace/aur-packages')
+PackagesFile.eachLine { line ->
+  packageName = line.trim()
+  println(packageName)
+    job(packageName) {
+      description("This Job builds the ${packageName} package for archlinux")
+      concurrentBuild()
+      label('master')
+      scm {
+        git("https://aur.archlinux.org/${line}.git")
+      }
+      steps {
+        shell("sudo arch-nspawn /mnt/aur/build_test/root pacman -Syyu; sudo /usr/bin/makechrootpkg -c -r /mnt/aur/build_test -l ${line}")
+      }
+    }
 }
