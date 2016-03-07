c2feb996f3364b0f72f24903f392b40fe8b29979
Tim Meusel <tim@bastelfreak.de>
2016-03-08 00:31:54 +0100

    reformat the groovy script, increase cron

--- create_subjobs.groovy
+++ create_subjobs.groovy
@@ -1,19 +1,18 @@
 def PackagesFile = new File('/var/lib/jenkins/jobs/Arch Package Builder/workspace/aur-packages')
 PackagesFile.eachLine { line ->
   packageName = line.trim()
-  println(packageName)
-    job(packageName) {
-      description("This Job builds the ${packageName} package for archlinux")
-      concurrentBuild()
-      label('master')
-      scm {
-        git("https://aur.archlinux.org/${line}.git")
-      }
-      triggers {
-        scm('*/15 * * * *')
-      }
-      steps {
-        shell("sudo /usr/bin/arch-nspawn /mnt/aur/build_test/root pacman -Syyu; sudo /usr/bin/makechrootpkg -c -r /mnt/aur/build_test -l ${line}")
-      }
+  job(packageName) {
+    description("This Job builds the ${packageName} package for archlinux")
+    concurrentBuild()
+    label('master')
+    scm {
+      git("https://aur.archlinux.org/${line}.git")
     }
+    triggers {
+      scm('*/20 * * * *')
+    }
+    steps {
+      shell("sudo /usr/bin/arch-nspawn /mnt/aur/build_test/root pacman -Syyu; sudo /usr/bin/makechrootpkg -c -r /mnt/aur/build_test -l ${line}")
+    }
+  }
 }
