56f32dbae90c861e60d837518921c6181812354b
Tim Meusel <tim@bastelfreak.de>
2016-03-08 15:28:47 +0100

    deploy artifacts to /var/www/archlinux

--- create_subjobs.groovy
+++ create_subjobs.groovy
@@ -1,20 +1,30 @@
 def PackagesFile = new File('/var/lib/jenkins/jobs/Arch Package Builder/workspace/aur-packages')
 PackagesFile.eachLine { line ->
   packageName = line.trim()
-  job(packageName) {
+  job("Arch Package ${packageName}") {
     description("This Job builds the ${packageName} package for archlinux")
     concurrentBuild()
     label('master')
     scm {
-      git("https://aur.archlinux.org/${line}.git")
+      git("https://aur.archlinux.org/${packageName}.git")
     }
     triggers {
-      scm('*/20 * * * *')
+      scm('H/20 * * * *')
     }
     steps {
-    // updates are fine, but there is currently no working locking
-    //shell("sudo /usr/bin/arch-nspawn /mnt/aur/build_test/root pacman -Syyu; sudo /usr/bin/makechrootpkg -c -r /mnt/aur/build_test -l ${line}")
-    shell("sudo /usr/bin/makechrootpkg -c -r /mnt/aur/build_test -l ${line}")
+      // updates are fine, but there is currently no working locking
+      //shell("sudo /usr/bin/arch-nspawn /mnt/aur/build_test/root pacman -Syyu; sudo /usr/bin/makechrootpkg -c -r /mnt/aur/build_test -l ${packageName}")
+      shell("sudo /usr/bin/makechrootpkg -c -r /mnt/aur/build_test -l ${packageName}")
+      def folder = new File("/var/www/archlinux/${packageName}/")
+      if( !folder.exists() ) {
+        folder.mkdirs()
+      }
+      artifactDeployer {
+        includes('*.pkg.tar.xz')
+        remoteFileLocation("/var/www/archlinux/${packageName}/")
+        failIfNoFiles()
+        deleteRemoteArtifacts()
+      }
     }
   }
 }
