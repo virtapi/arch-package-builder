8d4fd44cafe76d0d9585e7cc660484e9f7364d1d
Tim Meusel <tim@bastelfreak.de>
2016-03-26 16:01:18 +0100

    add chuck norris quotes
    based on
    https://jenkinsci.github.io/job-dsl-plugin/#path/javaposse.jobdsl.dsl.DslFactory.job-publishers-chucknorris

--- create_subjobs.groovy
+++ create_subjobs.groovy
@@ -45,11 +45,13 @@ PackagesFile.eachLine { line ->
       }
       postBuildScripts {
         steps {
+          // how to we run this for ever artefact?
           //shell('/usr/bin/repo-add --new --quiet /var/www/archlinux/aur/os/x86_64/repo.db.tar.gz /var/www/archlinux/aur/*.pkg.tar.xz')
           shell("sudo /usr/bin/btrfs subvolume delete /mnt/aur/build_test/${packageName}")
         }
         onlyIfBuildSucceeds(true)
       }
+      chucknorris()
     }
   }
   //queue("Arch_Package_${packageName}")
