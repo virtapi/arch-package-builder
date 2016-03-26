aea01791c1b780557c026624f99d37ce8a9c2066
Tim Meusel <tim@bastelfreak.de>
2016-03-26 01:01:23 +0100

    initial commit

---
+++ create-repo-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##
+# written by bastelfreak
+##
+# this script creates the initial db for our repo
+# you can rerun this after every change to the repo, but thats slow
+##
+
+cd /var/www/archlinux/aur/os/x86_64 || exit
+find . -type f -name "*.pkg.tar.xz" -exec repo-add --new aur.db.tar.gz {} \;
